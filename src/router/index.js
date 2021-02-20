acfab13b42c706bd4127d168bd4570532f9a5ee1
Tsing <shaoyu10@foxmail.ccom>
2021-02-20 15:33:55 +0800

    用户登录页面和路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+//在VueCLI中 @ 表示 src 的别名
+import Login from '@/views/login'
 
 //路由配置表
 const routes = [
-  
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
 ]
 
 const router = createRouter({
---
+++ src/utils/request.js
@@ -0,0 +1,17 @@
+/**
+ * 基于 axios 的请求模块
+ */
+import axios from 'axios'
+//创建axois实例
+//通过实例发送请求,把需要的配置发送给这个实例
+const request = axios.create({
+    baseURL: 'ttapi.research.itcast.cn/'//请求的基础路径
+})
+
+
+//请求拦截器
+
+//响应拦截器
+
+//导出请求包
+export default request
